246f1ae88db6dbf43a9e0bd826d180c3942e866a
Micheal Mithra <mmichealmithra@gmail.com>
2026-08-14 16:25:04 +0530

    api for user mobile

--- client-repository/frontend/user_mobile/lib/core/constants/api_constants.dart
+++ client-repository/frontend/user_mobile/lib/core/constants/api_constants.dart
@@ -1,16 +1,37 @@
+// import 'package:flutter/foundation.dart';
+
+// class ApiConstants {
+//   static String get baseUrl {
+//     if (kIsWeb) {
+//       return 'http://localhost:5000/api';
+//     }
+
+//     if (defaultTargetPlatform == TargetPlatform.android) {
+//       return 'http://10.0.2.2:5000/api';
+//     }
+
+//     return 'http://localhost:5000/api';
+//   }
+
+//   static String get mobileLogin => '$baseUrl/mobile/login';
+
+//   static String get categories => '$baseUrl/mobile/categories';
+
+//   static String get clientsByCategory => '$baseUrl/mobile/clients';
+
+//   static String clientDeliverables({
+//     required int clientId,
+//     required String categorySlug,
+//   }) {
+//     return '$baseUrl/mobile/clients/$clientId/deliverables?category=$categorySlug';
+//   }
+// }
+
 import 'package:flutter/foundation.dart';
 
 class ApiConstants {
   static String get baseUrl {
-    if (kIsWeb) {
-      return 'http://localhost:5000/api';
-    }
-
-    if (defaultTargetPlatform == TargetPlatform.android) {
-      return 'http://10.0.2.2:5000/api';
-    }
-
-    return 'http://localhost:5000/api';
+    return 'https://client.godigitalindia.co/api';
   }
 
   static String get mobileLogin => '$baseUrl/mobile/login';
