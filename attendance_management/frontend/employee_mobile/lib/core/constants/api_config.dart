8ded954125aed41808e74b60993f8e74c5cb29f0
Micheal Mithra <mmichealmithra@gmail.com>
2026-08-18 16:55:26 +0530

    port 3000

--- attendance_management/frontend/employee_mobile/lib/core/constants/api_config.dart
+++ attendance_management/frontend/employee_mobile/lib/core/constants/api_config.dart
@@ -1,41 +1,61 @@
 import 'package:flutter/foundation.dart';
 
 class ApiConfig {
+  // ==============================
+  // BASE URL
+  // ==============================
+
   static String get baseUrl {
+    // Flutter Web
     if (kIsWeb) {
+      // Local Web
       if (Uri.base.host == 'localhost' ||
           Uri.base.host == '127.0.0.1') {
-        return 'http://localhost:5002/api';
+        return 'http://localhost:3000/api';
       }
 
+      // Production Web
       return 'https://attendance.godigitalindia.co/api';
     }
 
-    // Release APK
+    // Android Release APK
     if (kReleaseMode) {
       return 'https://attendance.godigitalindia.co/api';
     }
 
-    // Debug Android Emulator
-    return 'http://10.0.2.2:5002/api';
+    // Android Debug - Emulator
+    return 'http://10.0.2.2:3000/api';
+
+    // Android Debug - Physical Phone
+    // return 'http://YOUR_PC_IP:3000/api';
   }
 
+  // ==============================
+  // SOCKET URL
+  // ==============================
+
   static String get socketUrl {
+    // Flutter Web
     if (kIsWeb) {
+      // Local Web
       if (Uri.base.host == 'localhost' ||
           Uri.base.host == '127.0.0.1') {
-        return 'http://localhost:5002';
+        return 'http://localhost:3000';
       }
 
+      // Production Web
       return 'https://attendance.godigitalindia.co';
     }
 
-    // Release APK
+    // Android Release APK
     if (kReleaseMode) {
       return 'https://attendance.godigitalindia.co';
     }
 
-    // Debug Android Emulator
-    return 'http://10.0.2.2:5002';
+    // Android Debug - Emulator
+    return 'http://10.0.2.2:3000';
+
+    // Android Debug - Physical Phone
+    // return 'http://YOUR_PC_IP:3000';
   }
 }
